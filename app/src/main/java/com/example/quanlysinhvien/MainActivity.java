a9f43a84dc3d4162831391b9f47b0a89ad448469
admin <daominhhiep97@gmail.com>
2020-08-07 17:01:35 +0700

    thêm chức năng đọc và ghi file

--- app/src/main/java/com/example/quanlysinhvien/MainActivity.java
+++ app/src/main/java/com/example/quanlysinhvien/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.quanlysinhvien;
 
 import android.content.DialogInterface;
+import android.os.Environment;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.*;
@@ -8,6 +9,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -26,10 +28,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        listStudent = read();
         initView();
         list();
-
         adapter = new StudentAdapter(this, R.layout.row_student, listStudent);
         listViewStudent.setAdapter(adapter);
 
@@ -39,28 +42,58 @@ public class MainActivity extends AppCompatActivity {
         onClickButtonSort();
         onClickListView();
         onLongClickListView();
-
-
     }
 
     private void onCheckGender() {
         groupGender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                switch (i){
+                switch (i) {
                     case R.id.checkMale:
-                        Toast.makeText(MainActivity.this,"Giới tính nam", Toast.LENGTH_SHORT);
                         break;
                     case R.id.checkFemale:
-                        Toast.makeText(MainActivity.this,"Giới tính nữ", Toast.LENGTH_SHORT);
                         break;
                 }
             }
         });
     }
 
+    private int getGender() {
+        int gender = 0;
+        if (checkMale.isChecked()) {
+            gender = R.drawable.male;
+        }
+        if (checkFemale.isChecked()) {
+            gender = R.drawable.female;
+        }
+        return gender;
+    }
 
-    private void confirmDelete(final int location){
+    private void onClickListView() {
+        listViewStudent.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                editTextId.setText(listStudent.get(i).getId());
+                editTextName.setText(listStudent.get(i).getName());
+                editTextBirth.setText(listStudent.get(i).getBirth());
+                editTextAddress.setText(listStudent.get(i).getAddress());
+                editTextGpa.setText(listStudent.get(i).getGpa());
+                location = i;
+            }
+        });
+    }
+
+    private void onLongClickListView() {
+        listViewStudent.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                confirmDelete(i);
+                return false;
+            }
+        });
+    }
+
+    private void confirmDelete(final int location) {
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
         alertDialog.setTitle("Thông Báo");
         alertDialog.setMessage("Bạn có muốn xóa thông tin sinh viên này không?");
@@ -68,6 +101,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 listStudent.remove(location);
+                write(listStudent);
                 adapter.notifyDataSetChanged();
             }
         });
@@ -80,6 +114,7 @@ public class MainActivity extends AppCompatActivity {
         alertDialog.show();
     }
 
+
     private void onClickButtonSort() {
         buttonSort.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -88,6 +123,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 
     private void showMenuSort() {
         PopupMenu popupMenu = new PopupMenu(this, buttonSort);
@@ -111,13 +147,19 @@ public class MainActivity extends AppCompatActivity {
         popupMenu.show();
     }
 
-
-    private void onLongClickListView() {
-        listViewStudent.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+    private void onClickButtonAdd() {
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                confirmDelete(i);
-                return false;
+            public void onClick(View view) {
+                String id = editTextId.getText().toString();
+                String name = editTextName.getText().toString();
+                String birth = editTextBirth.getText().toString();
+                String address = editTextAddress.getText().toString();
+                String gpa = editTextGpa.getText().toString();
+                int gender = getGender();
+                listStudent.add(new Student(id, name, birth, address, gpa, gender));
+                write(listStudent);
+                adapter.notifyDataSetChanged();
             }
         });
     }
@@ -131,45 +173,10 @@ public class MainActivity extends AppCompatActivity {
                 String birth = editTextBirth.getText().toString();
                 String address = editTextAddress.getText().toString();
                 String gpa = editTextGpa.getText().toString();
-
-                listStudent.add(new Student(id, name, birth, address, gpa));
-                listStudent.remove(location);
-                adapter.notifyDataSetChanged();
-            }
-        });
-    }
-
-    private void onClickListView() {
-        listViewStudent.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                editTextId.setText(listStudent.get(i).getId());
-                editTextName.setText(listStudent.get(i).getName());
-                editTextBirth.setText(listStudent.get(i).getBirth());
-                editTextAddress.setText(listStudent.get(i).getAddress());
-                editTextGpa.setText(listStudent.get(i).getGpa());
-                location = i;
-            }
-        });
-    }
-
-    private void onClickButtonAdd() {
-        buttonAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String id = editTextId.getText().toString();
-                String name = editTextName.getText().toString();
-                String birth = editTextBirth.getText().toString();
-                String address = editTextAddress.getText().toString();
-                String gpa = editTextGpa.getText().toString();
-                int  gender = 0;
-                if(checkMale.isChecked()){
-                    gender = R.drawable.male;
-                }
-                if (checkFemale.isChecked()){
-                    gender = R.drawable.female;
-                }
+                int gender = getGender();
                 listStudent.add(new Student(id, name, birth, address, gpa, gender));
+                listStudent.remove(location);
+                write(listStudent);
                 adapter.notifyDataSetChanged();
             }
         });
@@ -191,37 +198,36 @@ public class MainActivity extends AppCompatActivity {
 
     private void list() {
         listViewStudent = (ListView) findViewById(R.id.listViewStudent);
-        listStudent = new ArrayList<>();
-        listStudent.add(new Student("1", "Nguyễn Trung Hiếu", "24", "Hà Nội", "3.0", R.drawable.female));
-        listStudent.add(new Student("2", "Đào Minh Hiệp", "23", "Hà Nội", " 8.5", R.drawable.batman));
-        listStudent.add(new Student("3", "Nguyễn Đông Nam", "23", "Đông Anh", "6.0", R.drawable.male));
-        listStudent.add(new Student("4", "Lê Đức Tâm", "28", "Hà Nội", "9.0", R.drawable.male));
-        listStudent.add(new Student("5", "Nguyễn Văn Dũng", "23", "Hà Nội", "6.5", R.drawable.female));
+        listStudent = read();
     }
 
+    private void write(List<Student> studentList) {
+        try {
+            FileOutputStream fileOutputStream = this.openFileOutput(simpleFileName, MODE_PRIVATE);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(studentList);
+            objectOutputStream.close();
+            System.out.println("luu thanh cong");
 
-//    private void saveData() {
-//        try {
-//            FileOutputStream fileOutputStream = this.openFileOutput(simpleFileName, MODE_APPEND);
-//            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-//            objectOutputStream.writeObject(listStudent);
-//            objectOutputStream.close();
-//            Toast.makeText(this, "File1  saved!", Toast.LENGTH_SHORT).show();
-//        } catch (Exception e) {
-//            Toast.makeText(this, "Error:" + e.getMessage(), Toast.LENGTH_SHORT).show();
-//        }
-//    }
-//
-//
-//    private void readData() {
-//        List<Student> studentList = new ArrayList<>();
-//        try {
-//            FileInputStream fileInputStream = this.openFileInput(simpleFileName);
-//            ObjectInputStream objectInputStream= this.(fileInputStream);
-//            studentList = (List<Student>) objectInputStream.readObject();
-//            objectInputStream.close();
-//        } catch (Exception e) {
-//            Toast.makeText(this,"Error:"+ e.getMessage(),Toast.LENGTH_SHORT).show();
-//        }
-//    }
+            Toast.makeText(this,"sjfshdfjsd",Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(this, "Error:" + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private List<Student> read() {
+        List<Student> studentList = new ArrayList<>();
+
+        try {
+            FileInputStream fileInputStream = this.openFileInput(simpleFileName);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            studentList = (List<Student>) objectInputStream.readObject();
+            objectInputStream.close();
+
+            Toast.makeText(this,studentList.get(studentList.size()-1).getName() + studentList.get(studentList.size()-1).getGpa(),Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(this, "Error:" + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        return studentList;
+    }
 }
